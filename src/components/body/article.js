980e3f4de4ee5d40b6b6975a4a820b23de9c9512
karolhamuda <karol.hamuda@gmail.com>
2021-06-10 23:28:47 +0200

    small fix + initial article

---
+++ src/components/body/article.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Row } from 'react-bootstrap'
+import 
+
+
+
+
+export const article = () => {
+    return (
+        <>
+        <Row>
+            <Col>
+                <Row>
+
+                </Row>
+                <Row>
+
+                </Row>
+                <Row>
+                    <Col>
+                    </Col>
+                    <Col>
+                    </Col>
+                </Row>
+            </Col>
+
+            <Col>
+            </Col>
+        </Row>   
+        </>
+    )
+}

0f21c28cb89189dfebf8940c3c6a68f93d55a271
karolhamuda <karol.hamuda@gmail.com>
2021-10-13 21:47:27 +0200

    Article fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Article from '../components/body/Article.js'
+import { Article } from '../components/body/Article.js'
 import Slider from '../components/body/Slider/Slider.js'
 import '../fonts/typography.css';
 
